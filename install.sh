e163c522ca57acb0964d9b0b1e02fa06fa6004fc
JGMelon22 <jgvasconcellos22@gmail.com>
2023-09-29 16:17:18 -0300

    feat: improving script quality

--- install.sh
+++ install.sh
@@ -1,14 +1,39 @@
 #!/bin/bash
+wget https://desktop.replit.com/download/deb
+
+mv deb replit.deb
+
+ar x replit.deb
+
+zstd -d data.tar.zst
+
+tar -xvf data.tar
+
+cd usr/share/pixmaps/
+
+cp replit.png ../../lib/replit/resources/
+
+cd ../../
+cd lib/replit/
+
+chmod +x Replit
+
+cd ../ 
+
+# Copy replit electron code to /home direcortory
 cp -r replit ~/
-cd ~/replit/
-chmod +x Replit	
+
+cd ../../
+
+# Remove post install unnecessary files
+echo "Removing unnecessary files"
+rm -rf control.tar.zst data.tar data.tar.zst debian-binary replit.deb usr
 
 # Create desktop icon
 cd ~/.local/share/applications
 touch replit.desktop
 echo [Desktop Entry] > replit.desktop
 echo Encoding=UTF-8 >> replit.dsktop
-echo Version=1.0.2 >> replit.desktop # Change version here on update
 echo Name=Replit >> replit.desktop
 echo Path=~/ >> replit.desktop
 echo Exec=~/replit/Replit >> replit.desktop
